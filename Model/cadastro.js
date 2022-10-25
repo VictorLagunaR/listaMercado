cda8425016386ee7d85c883a72b5600f6b8aa75f
VictorLagunaR <laguna.vitorc@gmail.com>
2022-10-25 14:06:51 -0300

    arrumando verificacao no campo senha

--- Model/cadastro.js
+++ Model/cadastro.js
@@ -3,13 +3,14 @@
 const inputs = document.querySelectorAll(".form__input");
 
 //recebendo o spam de mensagens do input de confirmação de senha
-const mensagem = document.querySelector(".confirm")
+const mensagemConfirm = document.querySelector(".confirm")
+const mensagemSenha = document.querySelector(".password")
 
-//recebendo valor das senhas digitadas pelo o usuario
-var password = document.querySelector("#password");
-password = password.value;
-var passwordConfirm = document.querySelector("#passwordConfirm").value;
-passwordConfirm = passwordConfirm.value;
+//recebendo as tags das senhas
+const password = document.querySelector("#password");
+
+const passwordConfirm = document.querySelector("#passwordConfirm");
+
 
 const submit = document.querySelector(".btnCadastrar");
 
@@ -18,31 +19,62 @@ const submit = document.querySelector(".btnCadastrar");
 
 
 submit.addEventListener('click',function verificacao(event){
+    //recebendo valor das senhas digitadas pelo o usuario
+    const senha = password.value;
+    const senhaConfirm = passwordConfirm.value;
 
-    if(password == passwordConfirm){
-        password.classList.remove("erro");
-        passwordConfirm.classList.remove("erro");
-        password.classList.add("sucess");
-        passwordConfirm.classList.add("sucess");
-        
-        for (let i = 0; i < inputs.length; i++) {
+    //verificando os inputs se estão preenchidos
+    for (let i = 0; i < inputs.length; i++) {
         if (inputs[i].value.length == 0) {
             event.preventDefault()
             inputs[i].parentNode.classList.add("erro")
             submit.classList.add("notSubmit")
-            console.log(password)
-            console.log(passwordConfirm)
         }
         else{
             inputs[i].parentNode.classList.remove("erro")
             inputs[i].parentNode.classList.add("sucess")
         }
     }
+    console.log(password.parentNode);
+
+    //verificação das senhas se são iguais e se possuem no  mínimo oito caracterers
+    if(senha == senhaConfirm && senha.length > 7){
+        console.log("on")
+        password.parentNode.classList.remove("erro");
+        passwordConfirm.parentNode.classList.remove("erro");
+        password.parentNode.classList.add("sucess");
+        passwordConfirm.parentNode.classList.add("sucess");
+
+        mensagemConfirm.textContent = "";
+        mensagemSenha.innerHTML = "";
+    }
+    else if(senha != senhaConfirm){
+            
+            const mensagemSenha = document.querySelector(".password")
+
+            event.preventDefault();
+
+            submit.classList.add("notSubmit")
+            password.parentNode.classList.remove("sucess");
+            passwordConfirm.parentNode.classList.remove("sucess");
+            passwordConfirm.parentNode.classList.add("erro");
+            password.parentNode.classList.add("erro");
+            mensagemSenha.innerHTML = "";
+            mensagemConfirm.textContent = "As senhas não são iguais";
+            if (senha.length < 8) {
+                mensagemSenha.innerHTML = "A senha deve ter no mínimo oito caracteres!";
+            }
+    }
     else{
-        event.preventDefault()
-        password.classList.add("erro");
-        passwordConfirm.classList.add("erro");
-        mensagem.textContent = "As senhas não são iguais";
+        event.preventDefault();
+
+        submit.classList.add("notSubmit")
+        password.parentNode.classList.remove("sucess");
+        passwordConfirm.parentNode.classList.remove("sucess");
+        passwordConfirm.parentNode.classList.add("erro");
+        password.parentNode.classList.add("erro");
+        mensagemConfirm.textContent = "";
+        mensagemSenha.innerHTML = "A senha deve ter no mínimo oito caracteres!";
     }
 
 })
