1c3b6fba858e8844b888d12c359d5b814f8fde00
Victor Laguna <95255235+VictorLagunaR@users.noreply.github.com>
2022-11-09 10:57:51 -0300

    Update adicionar-item.js
    js

--- Model/adicionar-item.js
+++ Model/adicionar-item.js
@@ -1,5 +1,29 @@
-const button = document.querySelector(".container__adicionar-lista") 
+const adicionarLista = document.querySelector(".container__adicionar-lista") 
+const form = document.querySelector("#adicionar-lista");
+const excluir = document.querySelector(".excluir");
+const adicionar = document.querySelector(".submit");
+const nomeLista = document.querySelector(".adicionar__input");
+const erro = document.querySelector(".mensagemErro");
+adicionarLista.addEventListener('click', function(){
+    form.style.display = "flex";
+    adicionarLista.style.display = "none";
 
-button.addEventListener('click', function(){
-    window.location.href ="adicionar-lista.html";
+})
+
+
+
+excluir.addEventListener('click', function (e) {
+    e.preventDefault();
+    form.style.display = "none";
+    adicionarLista.style.display = "flex";
+    nomeLista.value = "";
+    erro.style.display = "none";
+})
+
+adicionar.addEventListener('click',function (event) {
+        
+    if (nomeLista.value.length == 0) {
+        event.preventDefault();
+        erro.style.display = "block";
+    }
 })
